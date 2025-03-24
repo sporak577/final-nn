c72dbc7d02b48c796212c39978703047ded584b0
sporak577 <sophie.porak@ucsf.edu>
2025-03-24 15:44:30 -0700

    minor changes

--- nn/__init__.py
+++ nn/__init__.py
@@ -3,15 +3,4 @@ BMI203: Biocomputing Algorithms - Winter 2023
 Final project: neural networks
 """
 from .nn import nn
-from setuptools import setup, find_packages
 
-setup(
-    name="nn",
-    version=("0.1.0"),
-    packages=find_packages(),
-    install_requires=[
-        "numpy"
-    ],
-    author="Sophie-Christine Porak",
-    description="a simple neural network built from scratch in numpy"
-)
